681211a96cbcb22d762e73b0f9f0985ccd1e8809
BarkeyZ <42813884+BarkeyZ@users.noreply.github.com>
2020-02-24 13:36:19 -0500

    changed to .html for automatic redirection here

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "Repository for this site: <a href='https://github.com/BarkeyZ/CIS355'>https://github.com/BarkeyZ/CIS355</a><br>";
-echo "<a href='helloworld.php'>Hello World</a></br>";
-echo "<a href='showarrays.php'>Show Arrays</a></br>";
-echo "<a href='banas.php'>Banas</a><br><br>";
-echo "<a href='/Database/events_people.php'>Database Test</a>"
-?>
